27155764bdd3bf6d5162c1aaf79f0c785234f902
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 12:03:45 +0200

    Created a basic HelloWorldController method which return an HelloWorld Object

--- src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
+++ src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.gbloch.todospringback.controllers;
 
+import com.gbloch.todospringback.model.HelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-model")
+    public HelloWorld helloWorldModel() {
+        return new HelloWorld("Hello World");
     }
 }
---
+++ src/main/java/com/gbloch/todospringback/model/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.gbloch.todospringback.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Gaëtan Bloch
+ * Created on 02/05/2020
+ */
+@Data
+@AllArgsConstructor
+public class HelloWorld {
+    private String message;
+}
