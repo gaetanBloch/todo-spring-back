5374fdafaef8862f2061d7631daf47b0137fe956
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 12:50:11 +0200

    Allow Cross Origin Resource Sharing (CORS) request from http://localhost:4200

--- src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
+++ src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.gbloch.todospringback.controllers;
 
 import com.gbloch.todospringback.model.HelloWorld;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Gaëtan Bloch
  * Created on 02/05/2020
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello-world")
