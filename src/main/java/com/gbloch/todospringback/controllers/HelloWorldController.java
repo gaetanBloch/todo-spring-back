c12be42b5e4518a8f6e30b3b015d4208ed880f74
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 14:25:24 +0200

    Changed the returned string

--- src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
+++ src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
@@ -13,16 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World from the Server";
     }
 
     @GetMapping("/hello-world-model")
     public HelloWorld helloWorldModel() {
-        return new HelloWorld("Hello World");
+        return new HelloWorld("Hello World from the Server");
     }
 
     @GetMapping("/hello-world/{name}")
     public HelloWorld helloWorldName(@PathVariable String name) {
-        return new HelloWorld(String.format("Hello World, %s", name));
+        return new HelloWorld(String.format("Hello World from the server %s", name));
     }
 }
