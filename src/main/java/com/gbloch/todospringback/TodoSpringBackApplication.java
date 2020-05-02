ce9963c1def22a322d3539a9d184c34501078032
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 11:30:42 +0200

    Initialized Spring Boot Project

---
+++ src/main/java/com/gbloch/todospringback/TodoSpringBackApplication.java
@@ -0,0 +1,13 @@
+package com.gbloch.todospringback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSpringBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gbloch/todospringback/TodoSpringBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbloch.todospringback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
