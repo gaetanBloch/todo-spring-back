240f9f5f8084a03f90e8a9e4a72b32dcd20af77f
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 13:08:35 +0200

    Added /api Prefix to the HelloWorldController for the URI

--- src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
+++ src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
@@ -1,17 +1,15 @@
 package com.gbloch.todospringback.controllers;
 
 import com.gbloch.todospringback.model.HelloWorld;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Gaëtan Bloch
  * Created on 02/05/2020
  */
 @CrossOrigin(origins = "http://localhost:4200")
-@RestController
+@RestController()
+@RequestMapping("/api")
 public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld() {
