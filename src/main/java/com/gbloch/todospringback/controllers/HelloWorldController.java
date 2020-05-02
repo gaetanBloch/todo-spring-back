67a0f54475da9d5401b67cbcc12450ebb0d788b2
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 12:06:58 +0200

    Created HelloWorldController method which return an HelloWorld Object with a path variable

--- src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
+++ src/main/java/com/gbloch/todospringback/controllers/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.gbloch.todospringback.controllers;
 
 import com.gbloch.todospringback.model.HelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,5 +19,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-model")
     public HelloWorld helloWorldModel() {
         return new HelloWorld("Hello World");
+    }
+
+    @GetMapping("/hello-world/{name}")
+    public HelloWorld helloWorldName(@PathVariable String name) {
+        return new HelloWorld(String.format("Hello World, %s", name));
     }
 }
