6bbd3dc6259f1eb9aa98c62b5a213521aa09af76
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-03 18:46:54 +0200

    Added Spring Security configuration to disable CSRF and enable OPTION Requests

---
+++ src/main/java/com/gbloch/todospringback/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,28 @@
+package com.gbloch.todospringback.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Gaëtan Bloch
+ * Created on 03/05/2020
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+//                .formLogin().and()
+                .httpBasic();
+    }
+}
