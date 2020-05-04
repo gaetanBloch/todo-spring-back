12e6b161965d3e94e34f5ae69a993b37fad574b9
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-04 07:39:39 +0200

    Created AuthenticationException

---
+++ src/main/java/com/gbloch/todospringback/jwt/resource/AuthenticationException.java
@@ -0,0 +1,11 @@
+package com.gbloch.todospringback.jwt.resource;
+
+/**
+ * @author Gaëtan Bloch
+ * Created on 03/05/2020
+ */
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
