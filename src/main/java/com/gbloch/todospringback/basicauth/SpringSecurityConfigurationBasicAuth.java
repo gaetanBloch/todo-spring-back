2b636aa269b68e7c959972e8b0919c384d3a5c19
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-05 11:01:25 +0200

    Moved back the SpringSecurityConfigurationBasicAuth Config file

--- src/main/java/com/gbloch/todospringback/basicauth/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/gbloch/todospringback/basicauth/SpringSecurityConfigurationBasicAuth.java
@@ -1,17 +1,15 @@
-package com.gbloch.basicauth;
+package com.gbloch.todospringback.basicauth;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author Gaëtan Bloch
  * Created on 03/05/2020
  */
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
 
     @Override
